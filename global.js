a8456f4f9b67abae2cad4bbea028ea89de0ebf2b
INGRID <ingridcarolinelima@hotmail.com>
2020-01-31 12:13:06 -0300

    module global

---
+++ global.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+global.MyApp = Object.freeze({
+    saudacao() {
+        return 'Estou em todos os lugares'
+    },
+    nome: 'Sisttema legal'
+})
---
+++ globalClient.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MyApp.saudacao())
+
+MyApp.nome = 'Eita!'
+console.log(MyApp.nome)
