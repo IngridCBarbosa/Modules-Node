39d20ae4726dfd830d97a2c98c2acb102e844caa
INGRID <ingridcarolinelima@hotmail.com>
2020-01-31 11:26:40 -0300

    more exercise

--- employees/employees.js
+++ employees/employees.js
@@ -1,7 +1,20 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = e => e.pais === 'China'
+const women =  e => e.genero === 'F'
+
+const minorS = (employee, actualEmployee) => {
+    return employee.salario < actualEmployee.salario ? employee : actualEmployee
+}
+
 axios.get(url).then(response =>{
     const funcionarios =  response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
+
+  
+    const func = funcionarios.filter(chineses).filter(women).reduce(minorS)
+    console.log(func)
+  
 })
+
